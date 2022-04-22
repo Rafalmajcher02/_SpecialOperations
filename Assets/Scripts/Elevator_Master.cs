c09fcdefa041d212138fcefa23ad70fc9b8adef3
Husky Majcher <rm260799@falmouth.ac.uk>
2022-04-23 00:51:27 +0100

    Removed elevatorplayerdetector and cleared elevator master scripts

--- Assets/Scripts/Elevator_Master.cs
+++ Assets/Scripts/Elevator_Master.cs
@@ -4,21 +4,16 @@ using UnityEngine;
 
 public class Elevator_Master : MonoBehaviour
 {
-    public GameObject elevatorShaft;
-    public GameObject[] elevatorFloors;
-    public ElevatorPlayerDetector EPD;
+    public GameObject[] floors;
 
-    public int floorCount;
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        floorCount = elevatorFloors.Length;
-        Debug.Log(floorCount);
+        if (collision.transform.tag == "Player")
+        {
+            Debug.Log("Entered");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
