8949169ae1ae55c53eded15ae49081233d675fe8
Husky Majcher <rm260799@falmouth.ac.uk>
2022-05-09 07:58:14 +0100

    Working button spawning and despawning

--- Assets/Scripts/Elevator_Single.cs
+++ Assets/Scripts/Elevator_Single.cs
@@ -6,16 +6,27 @@ public class Elevator_Single : MonoBehaviour
 {
     public GameObject elevatorPanel;
     public GameObject elevatorShaft;
+    public Elevator_Shaft es;
+
+    public void Start()
+    {
+        es = transform.GetComponentInParent<Elevator_Shaft>();
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
             elevatorPanel.SetActive(true);
+            es.ElevatorButtonAdjuster();
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        elevatorPanel.SetActive(false);
+        //Turns off children
+        for (int i = 0; i < elevatorPanel.transform.childCount; i++)
+        {
+            elevatorPanel.transform.GetChild(i).gameObject.SetActive(false);
+        }
     }
 }
