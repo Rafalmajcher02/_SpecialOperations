cf7618af92bc566081b1438db4088c223cbf4180
Husky Majcher <rm260799@falmouth.ac.uk>
2022-05-17 13:31:53 +0100

    Added teleport option for when button is pressed (elevator shaft script)

--- Assets/Scripts/Elevator_Shaft.cs
+++ Assets/Scripts/Elevator_Shaft.cs
@@ -7,9 +7,12 @@ public class Elevator_Shaft : MonoBehaviour
     [Header("Elevator Settings")]
     [Range(1, 8)]
     public int elevatorNumber = 1;
+    private int floorY = 0;
+
 
     [Header("Drags")]
     public GameObject panel;
+    public GameObject player;
 
     public virtual void ElevatorButtonAdjuster()
     {
@@ -18,5 +21,19 @@ public class Elevator_Shaft : MonoBehaviour
         {
             panel.transform.GetChild(i).gameObject.SetActive(true);
         }
+    }
+
+    public void OnClick()
+    {
+        Vector2 playerLocation = new Vector2(player.transform.position.x, floorY);
+        player.transform.SetPositionAndRotation(playerLocation, player.transform.rotation);
+    }
+    public void click1()
+    {
+        floorY = 1 * 5;
+    }
+    public void click2()
+    {
+        floorY = 2 * 5;
     }
 }
