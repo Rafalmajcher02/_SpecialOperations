09b498803a638808d2740d4e3f4c312c20fe1456
Husky Majcher <rm260799@falmouth.ac.uk>
2022-07-14 13:55:25 +0100

    Created CharacterGenerator script
    Added void awake to chose if civ will be randomized or premade costume

---
+++ Assets/Scripts/CharacterGenerator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGenerator : MonoBehaviour
+{
+    public Sprite[] head;
+    public Sprite[] body;
+    public Sprite[] allBody;
+
+    private void Awake()
+    {
+        //Chose A or B
+        int xNom = Random.Range(0, 1);
+
+        //A will be for generating random civ's head and body from selection of sprites
+        if (xNom == 0)
+        {
+            int xNomHead = Random.Range(0, head.Length);
+            int xNomBody = Random.Range(0, body.Length);
+        }
+        //B will be for generating a whole civ in premade costume from selection of sprites
+        else
+        {
+            int xNomAllBody = Random.Range(0, allBody.Length);
+        }
+    }
+
+}
